dddb4e13b455f3d8a77e7f9b572e1e5cc194bdd4
Baris Akkurt <dbarisakkurt@gmail.com>
2016-10-01 11:24:53 +0300

    first test case added for part5 exercise1

--- Interpreter/PlusMinusMultiplyDivideInterpreter/InterpreterTest.py
+++ Interpreter/PlusMinusMultiplyDivideInterpreter/InterpreterTest.py
@@ -4,3 +4,12 @@ import unittest
 
 if __name__ == '__main__':
     unittest.main()
+
+class AddSubtractMultiplyDivideTestInterpreter(unittest.TestCase):
+
+    # Add Only Tests
+    def test_MultiplyOnly1(self):
+        lexer = Lexer("1*2*3")
+        interpreter = Interpreter(lexer)
+        result = interpreter.interpret();
+        self.assertEqual(6, result)
